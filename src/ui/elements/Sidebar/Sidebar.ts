af5352a1c6e8ac50d36f27dcdf35ef7eb79ebc37
Sami Mokaddem <mokaddem.sami@gmail.com>
2025-09-03 15:38:27 +0200

    chg: [ui:sidebar] Added support of edge selection

--- src/ui/elements/Sidebar/Sidebar.ts
+++ src/ui/elements/Sidebar/Sidebar.ts
@@ -22,7 +22,7 @@ export class Sidebar implements UIElement {
   <div class="pivotick-sidebar">
     <div class="pivotick-mainheader-panel"></div>
     <div class="pivotick-properties-panel">
-      <div class="pivotick-header-panel"><h4>Properties</h4></div>
+      <div class="pivotick-header-panel"><h4>Basic Properties</h4></div>
       <div class="pivotick-body-panel">
         No selection
         (should be hidden instead)
@@ -57,6 +57,12 @@ export class Sidebar implements UIElement {
         this.uiManager.graph.renderer.getGraphInteraction().on("unselectNode", (node: Node, element: any) => {
             this.clearNodeOverview()
         })
+        this.uiManager.graph.renderer.getGraphInteraction().on("selectEdge", (edge: Edge, element: any) => {
+            this.injectEdgeOverview(edge, element)
+        })
+        this.uiManager.graph.renderer.getGraphInteraction().on("unselectEdge", (edge: Edge, element: any) => {
+            this.clearNodeOverview()
+        })
     }
 
     private clearNodeOverview(): void {
@@ -88,7 +94,6 @@ export class Sidebar implements UIElement {
 </div>`
         const mainheaderContent = createHtmlTemplate(template) as HTMLDivElement
         const iconElem = mainheaderContent.querySelector(".pivotick-mainheader-icon");
-        const nodeinfoElem = mainheaderContent.querySelector(".pivotick-mainheader-nodeinfo");
         const nameElem = mainheaderContent.querySelector(".pivotick-mainheader-nodeinfo-name");
         const subtitleElem = mainheaderContent.querySelector(".pivotick-mainheader-nodeinfo-subtitle");
         const actionElem = mainheaderContent.querySelector(".pivotick-mainheader-nodeinfo-action");
@@ -111,8 +116,50 @@ export class Sidebar implements UIElement {
         if (subtitleElem) {
             subtitleElem.innerHTML = 'Optional subtitle or description'
         }
+
         if (this.mainHeaderPanel) {
             this.mainHeaderPanel.innerHTML = mainheaderContent.outerHTML
         }
+        requestAnimationFrame(() => {
+            this.mainHeaderPanel?.firstElementChild?.classList.add('enter-active')
+        })
+    }
+
+    private injectEdgeOverview(edge: Edge, element: any): void {
+        const fixedPreviewSize = 42
+        const template = `<div>
+    <div class="pivotick-mainheader-nodepreview">
+        <svg xmlns="http://www.w3.org/2000/svg" width="${fixedPreviewSize}" height="${fixedPreviewSize}" viewBox="0 0 24 24" style="filter: drop-shadow(0px 2px 1px #00000033);">
+            <g fill="none" stroke="currentColor" stroke-width="1.5">
+                <path stroke-linejoin="round" d="M8 6h1.78c2.017 0 3.025 0 3.534.241a2.5 2.5 0 0 1 1.211 3.276c-.229.515-.994 1.17-2.525 2.483c-1.53 1.312-2.296 1.968-2.525 2.483a2.5 2.5 0 0 0 1.211 3.276c.51.241 1.517.241 3.534.241H16" />
+                <path d="M2 6a3 3 0 1 0 6 0a3 3 0 0 0-6 0Zm14 12a3 3 0 1 0 6 0a3 3 0 0 0-6 0Z" />
+            </g>
+        </svg>
+    </div>
+    <div class="pivotick-mainheader-nodeinfo">
+        <div class="pivotick-mainheader-nodeinfo-name"></div>
+        <div class="pivotick-mainheader-nodeinfo-subtitle"></div>
+    </div>
+    <div class="pivotick-mainheader-nodeinfo-action">
+    </div>
+</div>`
+        const mainheaderContent = createHtmlTemplate(template) as HTMLDivElement
+        const nameElem = mainheaderContent.querySelector(".pivotick-mainheader-nodeinfo-name");
+        const subtitleElem = mainheaderContent.querySelector(".pivotick-mainheader-nodeinfo-subtitle");
+        const actionElem = mainheaderContent.querySelector(".pivotick-mainheader-nodeinfo-action");
+
+        if (nameElem) {
+            nameElem.innerHTML = edge.getData().label
+        }
+        if (subtitleElem) {
+            subtitleElem.innerHTML = 'Optional subtitle or description'
+        }
+
+        if (this.mainHeaderPanel) {
+            this.mainHeaderPanel.innerHTML = mainheaderContent.outerHTML
+        }
+        requestAnimationFrame(() => {
+            this.mainHeaderPanel?.firstElementChild?.classList.add('enter-active')
+        })
     }
 }
