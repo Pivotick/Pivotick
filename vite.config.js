7193811e2ebf2d1d2e74fa0d507f58a44b7b9598
Sami Mokaddem <mokaddem.sami@gmail.com>
2025-11-13 16:31:06 +0100

    chg: [build] Added vite config

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.js'),
+            name: 'Pivotick',
+            fileName: 'pivotick',
+            formats: ['es', 'umd'],
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: [''],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                },
+                exports: 'named'
+            },
+        },
+    },
+    publicDir: false
+})
